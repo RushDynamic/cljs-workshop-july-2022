63c1ba0963c0da90f08b4b25aabafcad64a002e8
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-07-08 15:23:01 +0530

    feat: added re-frame

--- src/moviedb/core.cljs
+++ src/moviedb/core.cljs
@@ -2,18 +2,28 @@
   (:require
    [reagent.core :as r]
    [reagent.dom :as d]
+   [re-frame.core :as rf]
    [ajax.core :refer [GET json-response-format]]))
 
 ;; -------------------------
 ;; Views
 
 (def movie-title (r/atom ""))
-(def omdb-resp (r/atom ""))
+;; (def omdb-resp (r/atom ""))
 
 (defn handle-omdb-resp [resp]
   (.log js/console "Response:" (:Title resp) (:Year resp))
-  (reset! omdb-resp resp)
-  (swap! omdb-resp assoc :loaded true))
+  (rf/dispatch [:save-omdb-resp resp]))
+
+(rf/reg-sub
+ :loaded-response?
+ (fn [db _]
+   (get-in db [:omdb-data :loaded])))
+
+(rf/reg-sub
+ :omdb-resp
+ (fn [db _]
+   (get-in db [:omdb-data :resp])))
 
 (defn home-page []
   [:div {:class "home-container"}
@@ -27,12 +37,19 @@
     [:input {:type "input"
              :on-change #(reset! movie-title (.-value (.-target %)))
              }]]
-   (when (:loaded @omdb-resp)
+   (let [loaded? (rf/subscribe [:loaded-response?])
+         omdb-resp (rf/subscribe [:omdb-resp])]
+     (when @loaded?
      [:<>
       [:img {:src (:Poster @omdb-resp)}]
       [:h2 (str "Year: " (:Year @omdb-resp))]
-      [:h4 (:Plot @omdb-resp)]])])
+      [:h4 (:Plot @omdb-resp)]]))
+   ])
 
+(rf/reg-event-db
+ :save-omdb-resp
+ (fn [db [_ resp]]
+   (update db :omdb-data assoc :resp resp :loaded true)))
 ;; -------------------------
 ;; Initialize app
 
